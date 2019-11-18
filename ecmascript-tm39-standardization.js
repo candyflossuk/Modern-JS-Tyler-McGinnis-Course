cea02e59d4f68fea0f5a8131e6e8603707d85808
Ross Humphrey <ross.a.humphrey@googlemail.com>
2019-11-18 21:29:17 +0000

    Added notes on ECMA and the process of adding a feature to ecmascript (js)

---
+++ ecmascript-tm39-standardization.js
@@ -0,0 +1,42 @@
+/* ECMA
+ ECMA - Standardization of information systems
+
+ECMA Versions:-
+> JS
+Standard
+  > ECMA-262
+Committe
+  > TC-39
+
+ECMAScript used to refer to Javascript (because Oracle)
+Official Standard of JS
+
+Stages:
+
+Stage 0 - Strawman
+> Reviewed at TC39 Meeting
+
+Stage 1 - Proposal
+> Champion identified
+> Describes problem it solves
+> Illustrative examples
+> High level api
+> identify concerns
+
+Stage 2 - DRAFT
+Description of syntax and semantics of new feature
+
+Stage 3- Candidate
+spec text finished
+2 spec compliant implementations created
+
+Stage 4 - finished
+Tests written
+2 spec implementation pass Tests
+practical experience
+spec editor signoff
+
+Release Schedule:
+EcmaScript released every year
+
+*/
