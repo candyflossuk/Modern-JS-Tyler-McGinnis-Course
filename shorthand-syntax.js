8e8fec35cd13b24f067fcdaae22c9935eaf286dd
Ross Humphrey <ross.a.humphrey@googlemail.com>
2019-11-26 20:38:32 +0000

    Added shorthand syntax example

---
+++ shorthand-syntax.js
@@ -0,0 +1,31 @@
+/*
+  Shorthand syntax
+
+  Property
+  Method names
+
+  Makes objects concise
+*/
+
+// Shorthand property names
+function formatMessage(name, id, avatar){
+  return {
+    name: name,
+    id: id,
+    avatar: avatar,
+    timestampe: Date.now()
+  }
+}
+
+// if key or property name is the same get rid of it
+
+function formatMessage(name, id, avatar){
+  return {
+    name,
+    id,
+    avatar,
+    timestampe: Date.now()
+  }
+}
+
+// Short hand method names
