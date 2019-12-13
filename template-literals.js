2a009bef817b1ba1447f84b356b9c9c0560a044f
Ross Humphrey <ross.a.humphrey@googlemail.com>
2019-12-13 12:31:01 +0000

    updating template-literals

--- template-literals.js
+++ template-literals.js
@@ -15,7 +15,7 @@ makeGreeting('Ross', 'ross.a.humphrey@gmail.com', '122')
 
 //new style
 function makeGreeting(name, email, id){
-  return `Hello, ${name}. We've emailed you at ${email}. Your user id is {id}`
+  return `Hello, ${name}. We've emailed you at ${email}. Your user id is ${id}`
 }
 
 // we can do the same with html
@@ -25,7 +25,7 @@ function makeGreetingTemplate(name, email, id){
     <div>
       <h1>Hello, ${name}</h1>
       <p>
-        We've emailed you at {email}
+        We've emailed you at ${email}
         Your user is is "${id}".
       </p>
     </div>
