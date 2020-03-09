2199e595db468139e66de4d88fb359b597463185
candyflossuk <ross.a.humphrey@googlemail.com>
2020-03-09 22:31:28 +0000

    Added classes syntax and showing how to use the class keyword

--- prototype-chain-es6-classes.js
+++ prototype-chain-es6-classes.js
@@ -262,6 +262,44 @@ keyword. If you leave off 'new' when you invoke the function, that 'this' object
 is it implicitly returned.
  */
 
+function Animal (name, energy) {
+  this.name = name
+  this.energy = energy
+}
+
+const leo = Animal('Leo', 7)
+console.log(leo) // defined
+
+// The name for this pattern is Pseudoclassical Instantation
+
+// in 2015 the 'class' keyword was added to the language.
+// Using this class syntax our class would look something like this
+
+class Animal {
+  constructor(name, energy) {
+    this.name = name
+    this.energy = energy
+  }
+
+  eat(amount) {
+    console.log(`${this.name} is eating.`)
+    this.energy += amount
+  }
+
+  sleep(length) {
+    console.log(`${this.name} is sleeping.`)
+    this.energy += length
+  }
+
+  play(length) {
+    console.log(`${this.name} is playing.`)
+    this.energy -= length
+  }
+}
+
+const leo = new Animal('Leo', 7)
+const snoop = new Animal('Scoop', 10)
+
 
 
 
